b4e171abbd10a8bfd4fef22fb924257a46e8e389
Ashish Yadav <aybhu10@gmail.com>
2024-11-12 20:42:49 +0530

    nested dynamic routing on click

--- pages/aboutus/[id].js
+++ pages/aboutus/[id].js
@@ -1,11 +1,5 @@
 import { useRouter } from 'next/router';
-
-const details = [
-    { id: 1, name: 'Yash', role: 'Senior Developer' },
-    { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
-    { id: 3, name: 'Suresh', role: 'Frontend Developer' }
-]
-
+import { details } from './data';
 
 function AboutUsMember() {
 
--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,5 +1,19 @@
-function AboutUsPage(){
-    return <h1>The About Us Page</h1>
+import { details } from './data';
+import Link from 'next/link';
+
+function AboutUsPage() {
+    return (
+        <div>
+            <ul>
+                {
+                    details.map(member =>
+                        <li key={member.id}>
+                            <Link href={`aboutus/${member.id}`}>{member.name}</Link>
+                        </li>)
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default AboutUsPage;
