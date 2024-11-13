189ee9e09728143877c90ff4cd954850ce85237b
Ashish Yadav <aybhu10@gmail.com>
2024-11-13 15:38:19 +0530

    pre-rendering the components

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,7 +2,7 @@ import MeetupDetails from "@/components/meetups/MeetupDetails";
 
 function MeetupDetail() {
 
-    return <MeetupDetails 
+    return <MeetupDetails
         title='The first meetup'
         image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
         address='Varanasi, UP, India'
@@ -10,4 +10,43 @@ function MeetupDetail() {
     />
 }
 
+export async function getStaticPaths(){
+
+    return {
+        fallback: false,
+        paths:[
+            {
+                params:{
+                    meetupId:'m1',
+                }
+            },
+            {
+                params:{
+                    meetupId:'m2',
+                }
+            },
+            {
+                params:{
+                    meetupId:'m3',
+                }
+            }
+        ]
+    }
+}
+export async function getStaticProps(context) {
+    
+    const meetupId=context.params.meetupId;
+
+    return {
+        props: {
+            meetupData: {
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+                id: meetupId,
+                title: 'The first meetup',
+                address: 'Varanasi, UP, India',
+                description: 'This is first meetup'
+            }
+        }
+    }
+}
 export default MeetupDetail;
--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,25 @@ const DUMMY_MEETUPS = [
     address:'Lucknow, UP, India',
     description:'This is second meetup'
 },
+{
+    id:'m3',
+    title:'The third meetup',
+    image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address:'Noida, UP, India',
+    description:'This is third meetup'
+},
 ]
-function HomePage(){
-    return <MeetupList meetups={DUMMY_MEETUPS} />
+function HomePage(props){
+    return <MeetupList meetups={props.meetups} />
+}
+
+export async function getStaticProps(){
+    // fetch data from an API
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        }
+    };
 }
 
 export default HomePage;
